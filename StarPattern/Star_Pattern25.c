955bb8e99131734b2dd3929b8aa3ae6f0905717c
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-05-16 18:45:54 +0700

    Update Star_Pattern25.c

--- StarPattern/Star_Pattern25.c
+++ StarPattern/Star_Pattern25.c
@@ -124,15 +124,13 @@ int main()
                 calculateForSurface(cubeX, cubeWidth, cubeY, '+');
             }
         }
-    printf("\x1b[H");
-    for (int k = 0; k < width * height; k++) {
-      putchar(k % width ? buffer[k] : 10);
-    }
+        printf("\x1b[H");
+        for (int k = 0; k < width * height; k++) putchar(k % width ? buffer[k] : 10);
 
-    A += 0.05;
-    B += 0.05;
-    C += 0.01;
-    usleep(8000 * 2);
-}
-  return 0;
+        A += 0.05;
+        B += 0.05;
+        C += 0.01;
+        usleep(8000 * 2);
+    }
+    return 0;
 }
